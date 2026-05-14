e49acc343f76e558fa09930f64ffb229f55dddaa
Anderson Chancusi <andersonjoelchancusi@gmail.com>
2026-05-13 19:35:13 -0500

    Conexion a la base de datos

---
+++ src/main/java/ec/edu/uce/Main.java
@@ -0,0 +1,25 @@
+package ec.edu.uce;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+
+@QuarkusMain
+public class Main {
+
+    public static void main(String[] args) {
+        Quarkus.run(App.class, args);
+
+    }
+
+    public static class App implements QuarkusApplication {
+        @Override
+        public int run(String... args) {
+            System.out.println("Conexion a base de datos");
+            return 0;
+
+        }
+    }
+
+}
