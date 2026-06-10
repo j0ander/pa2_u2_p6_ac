e14e7c1118a51a586ac1d5bd5207bf760a68aca9
Anderson <andersonjoelchancusi@gmail.com>
2026-06-10 18:10:41 -0500

    asdga

--- src/main/java/ec/edu/uce/Main.java
+++ src/main/java/ec/edu/uce/Main.java
@@ -25,7 +25,7 @@ public class Main {
         public int run(String... args) {
             //System.out.println("Conexion a base de datos");
             Estudiante est = new Estudiante("Anderson", "Chancusi", LocalDate.of(2005, 1, 29), "ads");
-            Estudiante est1 = new Estudiante(2,"Anderson", "Chancusi", LocalDate.of(2005, 1, 29), "Maculino");
+            //Estudiante est1 = new Estudiante(2,"Anderson", "Chancusi", LocalDate.of(2005, 1, 29), "Maculino");
             estudianteService.guardar(est);
             estudianteService.eliminar(3);
             //estudianteService.actualizar(est1);
